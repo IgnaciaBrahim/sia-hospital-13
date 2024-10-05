b52318e31e6296b884a258d88cd2a88e0320bc60
E. Ponce Fernández <eva.ponce.f@mail.pucv.cl>
2024-10-05 17:51:07 -0300

    Ventana Actualizada
    Añadir varios .json y pasar a español para que se vean las tildes y ñ

--- Hospital/src/hospital/Main.java
+++ Hospital/src/hospital/Main.java
@@ -1,6 +1,8 @@
 package hospital;
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class Main {
     public static void explicarOpcionesMenu() {
